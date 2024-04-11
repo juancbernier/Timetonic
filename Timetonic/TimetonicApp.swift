5b5045d19ef9841aa4b67c6df1287ee8a981c71e
Juancbernier <juanberniercr@unimagdalena.edu.co>
2024-04-11 18:52:48 -0500

    Initial Commit

---
+++ Timetonic/TimetonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetonicApp.swift
+//  Timetonic
+//
+//  Created by Juan Bernier on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TimetonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
